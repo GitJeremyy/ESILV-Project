55d57996be69476918528ebc4d47ba61d0926d7a
noahASD <37837626+Nh161103@users.noreply.github.com>
2024-11-19 19:21:53 +0100

    connecting database to server

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const mysql = require('mysql2'); // Ajouter MySQL
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'data'
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Erreur de connexion à la base de données :', err);
+    return;
+  }
+  console.log('Connecté à la base de données MySQL');
+});
 
 // Définir le dossier 'public' comme dossier statique
 app.use(express.static(path.join(__dirname, 'public')));
@@ -18,7 +34,21 @@ app.get('/reservation', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'Réservation.html'));
 });
 
+app.get('/reservations', (req, res) => {
+  const query = 'SELECT * FROM booking'; // Remplacez par le nom de votre table
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('Erreur lors de l\'exécution de la requête :', err);
+      res.status(500).send('Erreur serveur');
+      return;
+    }
+    res.json(results); // Envoyer les résultats en JSON
+  });
+});
+
 // Démarrer le serveur sur le port 3000
 app.listen(3000, () => {
   console.log('Serveur lancé sur http://localhost:3000');
 });
+
+
